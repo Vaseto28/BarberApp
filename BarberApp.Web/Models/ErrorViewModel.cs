532f102c40773ab4b2ca3206dba929199049582d
Vaseto28 <vasilkremenarovv@gmail.com>
2024-11-26 14:10:17 +0200

    Initial commit

---
+++ BarberApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BarberApp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
